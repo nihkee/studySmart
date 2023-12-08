e813bdd190ce65d06f2cdc556dfce3fb38570700
genghiskhang <khang11475@gmail.com>
2023-12-08 01:00:35 -0500

    email queue and sending emails setup

--- server.js
+++ server.js
@@ -3,14 +3,27 @@ const bodyParser = require("body-parser")
 const mongodb = require("mongodb")
 const cors = require("cors")
 const sqlite3 = require("sqlite3").verbose()
+const cron = require("node-cron")
+const fs = require("fs")
+const nodemailer = require("nodemailer")
 
 const app = express();
-let db = new sqlite3.Database("email_queue.db", (err) => {
+const db = new sqlite3.Database("email_queue.db", (err) => {
     if (err) {
         return console.error(err.message);
     }
     console.log("Connected to SQLite database");
 });
+
+const emailCredentials = JSON.parse(fs.readFileSync("email_credentials.json", "utf8"))
+const transporter = nodemailer.createTransport({
+
+    service: emailCredentials["service"],
+    auth: {
+        user: emailCredentials["user"],
+        pass: emailCredentials["pass"]
+    }
+})
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
@@ -21,6 +34,7 @@ app.get("/home", (request, response) => {
         a:"test response"
     })
 })
+
 // process preference data from request
 // respond with recommendation
 app.post("/preferences", (request, response) => {
@@ -47,20 +61,74 @@ app.post("/preferences", (request, response) => {
     }
 })
 
+// check and send emails every minute
+cron.schedule('* * * * *', () => {
+    console.log("Emails from last 30 minutes (for testing, will be after 30 minutes)")
+    db.each(`
+        SELECT *
+        FROM emails
+        WHERE timestamp >= Datetime('now', '-30 minutes');
+    `, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log(`${row.email} : ${row.timestamp}`)
+
+        transporter.sendMail({
+            from: emailCredentials["user"],
+            to: row.email,
+            subject: 'Test Feedback Form :)',
+            html: "<b> Hello World?</b>"
+        }, (error, info) => {
+            if (error) {
+                console.error(error.message);
+            }
+            console.log(`Message sent: ${info.messageId}`)
+        })
+
+        db.run(`DELETE FROM emails WHERE email = '${row.email}'`)
+    })
+})
+
+const insertEmail = async(email, currDatetime) => {
+    return new Promise((resolve, reject) => {
+        db.all(`SELECT email FROM emails WHERE email = '${email}'`, [], (err, rows1) => {
+            if (err) {
+                reject(err);
+            }
+            else if (rows1.length > 0) {
+                reject(new Error(`${email} already exists`))
+            }
+            else {
+                db.run(`
+                    INSERT INTO emails (
+                        email,
+                        timestamp
+                    )
+                    VALUES ("${email}", "${currDatetime}");`, (err) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    console.log(`${email} added to queue`)
+                    resolve({res:`${email} added to queue`});
+                })
+            }
+        })
+    })
+}
+
 //process user's email
-app.post("/emails", (request, response) => {
+app.post("/emails", async (request, response) => {
+    var email = request.body["email"];
+    var currDatetime = new Date().toISOString();
+
     try {
-        var email = request.body["email"];
-        console.log(email);
-        response.json({
-            res:`email ${email} received`
-        })
+        const result = await insertEmail(email, currDatetime);
+        response.json(result);
     }
-    catch (error) {
-        console.log(error)
-        response.json({
-            a:"poopoo response"
-        })
+    catch(error) {
+        console.error(error.message);
+        response.json({res:error.message})
     }
 })
 
